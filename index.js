492112ea25d8bc77b5a04ca98d11a7f80c1d1b54
JinBin Li <joysmshr@126.com>
2016-08-20 14:31:04 +0800

    webmasscan start

---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.execom;
+
+server.start(router.route, handle);
